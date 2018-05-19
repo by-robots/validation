d0c0749c79785d4bf12c15337ad14b616201fded
By Robots <skynet@by-robots.com>
2018-05-19 20:48:20 +0100

    Set the name in the present rule.

--- src/Rules/Present.php
+++ src/Rules/Present.php
@@ -9,6 +9,11 @@ class Present extends AbstractRule
     /**
      * @inheritDoc
      */
+    protected $name = 'present';
+
+    /**
+     * @inheritDoc
+     */
     public function validate($field, array $input, array $params = null): bool
     {
         return array_key_exists($field, $input);
