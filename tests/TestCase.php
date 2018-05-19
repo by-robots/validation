6d51f6c21e252c844f9e15d96a7c967fea9e537d
By Robots <skynet@by-robots.com>
2018-05-19 20:07:53 +0100

    Add test boilerplate.

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Set-up for testing.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        //
+    }
+
+    /**
+     * Tidy-up after ourselves.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        //
+    }
+}
