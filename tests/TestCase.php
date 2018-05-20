aad7a1153964dd5525aa06d486b4971e013baf51
By Robots <skynet@by-robots.com>
2018-05-20 19:53:27 +0100

    Clean-up the base test case.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,23 +4,5 @@ namespace Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * Set-up for testing.
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        //
-    }
-
-    /**
-     * Tidy-up after ourselves.
-     *
-     * @return void
-     */
-    public function tearDown()
-    {
-        //
-    }
+    //
 }
